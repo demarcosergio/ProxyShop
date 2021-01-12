f60eb11031b6fafe40f958829df6ce7f0e35a3dd
sdemarco <sdemarco@pagustech.com>
2021-01-11 23:35:35 -0300

    Updates

---
+++ precache-manifest.bde01d030c2f7226d7066ba86698bad5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d852d0669d4caca09638a3fe088dc88",
+    "url": "/ProxyShop/index.html"
+  },
+  {
+    "revision": "721d569bd237cf955fd2",
+    "url": "/ProxyShop/static/css/main.a2e3035b.chunk.css"
+  },
+  {
+    "revision": "d4d39c07352e6c1f4c4f",
+    "url": "/ProxyShop/static/js/2.f0ec671d.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/ProxyShop/static/js/2.f0ec671d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "721d569bd237cf955fd2",
+    "url": "/ProxyShop/static/js/main.d70c468d.chunk.js"
+  },
+  {
+    "revision": "067c52e0e315b4142430",
+    "url": "/ProxyShop/static/js/runtime-main.74e5c331.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ProxyShop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ProxyShop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
